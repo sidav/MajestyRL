3db03a03aa70806898cb830a1871b4f1d6c31f87
sidav <sidav94@gmail.com>
2020-08-31 02:44:44 +0300

    Reduces decal memory usage.

--- render_decals.go
+++ render_decals.go
@@ -5,21 +5,16 @@ import "github.com/sidav/golibrl/console"
 type decal struct {
 	appearance *ccell
 	x, y int
-	timesToRender int
-	lastTurnRendered int
+	lastTurnToBeRendered int
 }
 
 func (r *rendererStruct) renderDecalsInViewport() {
 	for i := range r.decalsBuffer {
 		decal := r.decalsBuffer[i]
-		if decal.timesToRender > 0 {
+		if decal.lastTurnToBeRendered >= getCurrentTurn() {
 			if r.areGlobalCoordsOnScreenForFaction(decal.x, decal.y, r.currentFactionSeeingTheScreen) {
 				sx, sy := decal.x - r.vx, decal.y - r.vy
 				r.renderCcellOnScreenCoords(r.decalsBuffer[i].appearance, sx, sy)
-				if decal.lastTurnRendered < getCurrentTurn() {
-					decal.lastTurnRendered = getCurrentTurn()
-					decal.timesToRender -= 1
-				}
 			}
 		}
 	}
@@ -27,7 +22,7 @@ func (r *rendererStruct) renderDecalsInViewport() {
 
 func (r *rendererStruct) cleanDecalsBuffer() {
 	for i := range r.decalsBuffer {
-		if r.decalsBuffer[i].timesToRender <= 0 {
+		if r.decalsBuffer[i].lastTurnToBeRendered < getCurrentTurn() {
 			r.decalsBuffer = append(r.decalsBuffer[:i], r.decalsBuffer[i+1:]...)
 			return
 		}
@@ -37,5 +32,5 @@ func (r *rendererStruct) cleanDecalsBuffer() {
 func addBasicDecalToRender(x, y, times int) {
 	RENDERER.decalsBuffer = append(RENDERER.decalsBuffer,
 		&decal{appearance: &ccell{color: console.RED, char: 'X'},
-		x: x, y: y, timesToRender: times, lastTurnRendered:getCurrentTurn()+1})
+		x: x, y: y, lastTurnToBeRendered: getCurrentTurn()+times})
 }
